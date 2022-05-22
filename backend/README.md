56762e47642d7305a17c8ffe83aec33585c25f5b
gabrielCaio <email.caiogabriel@gmail.com>
2022-05-22 11:47:46 -0300

    feat: prisma setup

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,7 @@
 # DevStar backend
+
+## Project Setup
+
+Install dependencies: ```yarn```
+
+Start docker: ```docker-compose up -d```
---
+++ backend/prisma/migrations/20220522142436_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "available" BOOLEAN NOT NULL DEFAULT true,
+    "authorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
