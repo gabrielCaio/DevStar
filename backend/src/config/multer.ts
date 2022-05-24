5bb46a15bdb94da66d29d451e9c57d54675f37fc
gabrielCaio <email.caiogabriel@gmail.com>
2022-05-24 09:32:41 -0300

    feat: new storage type to multer, buffer

---
+++ backend/src/config/multer.ts
@@ -0,0 +1,46 @@
+import multer from "multer";
+import path from "path"
+import crypto from 'crypto'
+
+const storageType = {
+    local: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, path.resolve(__dirname, "..", "..", "tmp"))
+        },
+        filename: (req, file, cb) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) cb(err, "")
+
+                let filekey = `${buf.toString("hex")}-${file.originalname}`
+
+                cb(null, filekey)
+            })
+        }
+    }),
+    memory: multer.memoryStorage()
+}
+
+const config = {
+    // dest: path.resolve(__dirname, "..", "..", "tmp"),
+    storage: storageType.memory,
+    limits: { fileSize: 50 * 1024 * 1024 },
+    fileFilter: (req: any, file: any, cb: any) => {
+        const allowedMimes = [
+            "image/jpeg",
+            "image/pjpeg",
+            "image/png",
+            "image/gif",
+            "video/mp4",
+            "video/mpeg",
+            "video/mkv"
+        ];
+
+        if (allowedMimes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error("Invalid file type"));
+        }
+    },
+}
+
+export const Multer = multer(config)
