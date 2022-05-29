c5cbc7904aa77ef2445b0a423ef2344bd1b77787
gabrielCaio <email.caiogabriel@gmail.com>
2022-05-29 18:42:39 -0300

    feat: register wireframe

--- frontend/dev-star/src/pages/Register/index.tsx
+++ frontend/dev-star/src/pages/Register/index.tsx
@@ -1,8 +1,68 @@
-import { Container } from './styles'
+import { useState } from 'react'
+import { Container, InputArea, ErrorMessage, LoginText } from './styles'
+import { TextInput, DefaultButton } from '../../components'
+
+interface UserData {
+    name: string,
+    email: string,
+    password: string,
+    confirmPassword: string
+}
+
+interface ErrorRegister {
+    error: boolean,
+    message: string
+}
 
 export default function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState<ErrorRegister>({ error: false, message: ""});
+
+    function registerUser() {
+        const userData: UserData = { name, email, password, confirmPassword }
+
+        const valid = validateData(userData)
+
+        if(valid.error) return setError(valid)
+
+        // TODO: Send data to backend
+        console.log(userData)
+    }
+
+    function handleKeyPress(e: any) {
+        if(e.key === 'Enter') registerUser();
+    }
+
+    function validateData(data: UserData): ErrorRegister {
+        
+        if(data.password !== data.confirmPassword) return { error: true, message: "Senhas não conferem" }
+        return { error: false, message: ""}
+    }
+
     return (
         <Container>
-            <h1>Register</h1>
+            <h1>Cadastro</h1>
+
+            <InputArea>
+                <TextInput placeholder='Nome' onChange={e => setName(e.target.value)} />
+                <TextInput placeholder='Email' onChange={e => setEmail(e.target.value)} />
+                <TextInput placeholder='Senha' onChange={e => setPassword(e.target.value)} />
+                <TextInput
+                    placeholder='Confirmar Senha'
+                    onChange={e => setConfirmPassword(e.target.value)}
+                    onKeyDown={handleKeyPress}
+                />
+            </InputArea>
+
+            <div>
+                <DefaultButton onClick={registerUser} >Realizar cadastro</DefaultButton>
+                {error.error && <ErrorMessage>{error.message}</ErrorMessage>}
+            </div>
+
+
+            <LoginText>Já possui conta? <p>Faça Login</p></LoginText>
         </Container>
     )}
--- frontend/dev-star/src/pages/Register/styles.ts
+++ frontend/dev-star/src/pages/Register/styles.ts
@@ -5,6 +5,42 @@ export const Container = styled.div`
     height: 100vh;
     display: flex;
     flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+`
+
+export const InputArea = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+export const ErrorMessage = styled.h3`
+    font-size: 0.8rem;
+    color: #FC492B;
+`
+
+export const LoginText = styled.h3`
+    display: flex;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
-`;
+    font-size: 1rem;
+    cursor: pointer;
+    transition: 0.5s;
+
+    &:hover {
+        opacity: 0.8;
+    }
+
+    p {
+        margin-left: 5px;
+    }
+`
