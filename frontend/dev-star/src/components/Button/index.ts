f8027ba4d45f7bd4ae09aef758ff79e9480b153c
gabrielCaio <email.caiogabriel@gmail.com>
2022-06-02 20:05:40 -0300

    feature: close issue #6

--- frontend/dev-star/src/components/Button/index.ts
+++ frontend/dev-star/src/components/Button/index.ts
@@ -1,11 +1,25 @@
 import styled from 'styled-components'
 
-export const DefaultButton = styled.button`
+import { colors } from '../../utils'
+
+interface ButtonProps {
+    backgroundColor?: string,
+    fontWeight?: string,
+    color?: string,
+}
+
+export const DefaultButton = styled.button<ButtonProps>`
+    width: 100%;
     border: none;
-    border-radius: 5px;
+    border-radius: 10px;
     padding: 1rem;
     cursor: pointer;
     transition: 0.5s;
+
+    background-color: ${p => p.backgroundColor ? p.backgroundColor : colors.orange};
+    font-weight: ${p => p.fontWeight ? p.fontWeight : "bold"};
+    color: ${p => p.color ? p.color : "#f2f2f2"};
+    font-size: 1em;
 
     &:hover {
         filter: brightness(0.8);
--- frontend/dev-star/src/pages/Login/styles.ts
+++ frontend/dev-star/src/pages/Login/styles.ts
@@ -1,9 +1,4 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    width: 70%;
-    background-color: #0c7eb6;
-    display: flex;
-    align-items: center;
-    justify-content: center;
 `;
