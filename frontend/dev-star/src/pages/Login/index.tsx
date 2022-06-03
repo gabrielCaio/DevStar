445909c5e03302b483a8113f564b67430dfaaa2b
gabrielCaio <email.caiogabriel@gmail.com>
2022-06-02 21:17:15 -0300

    feat: component input created

--- frontend/dev-star/src/pages/Login/index.tsx
+++ frontend/dev-star/src/pages/Login/index.tsx
@@ -1,10 +1,13 @@
 import { Container } from './styles'
 import { DefaultButton } from '../../components'
 
+import { TextInput } from '../../components'
+
 export default function Login() {
     return (
         <Container>
             <h1>Login</h1>
+            <TextInput placeholder='Email' />
         </Container>
     )
 }
