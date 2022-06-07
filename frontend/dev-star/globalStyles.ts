aae7ef887201d4bc6f651ba561d2548f8bf52454
gabrielCaio <email.caiogabriel@gmail.com>
2022-06-07 13:29:00 -0300

    fix: box sizing

--- frontend/dev-star/globalStyles.ts
+++ frontend/dev-star/globalStyles.ts
@@ -3,12 +3,9 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
     * {
         font-family: 'Lora', serif;
-    }
-    body {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: Open-Sans, Helvetica, Sans-Serif;
     }
 `;
 
